faa5c673ac7087f8f24cc3aa7c159d18de11d7e4
JosueEmanuelDionicioOrellana <Jdionicioo@miummg.edu.gt>
2022-03-13 23:15:09 -0600

    Inicializamos el proyecto

---
+++ notacion-posfija/src/main/java/gt/edu/umg/progra3/parcial1/pilas/NotationParser.java
@@ -0,0 +1,24 @@
+package gt.edu.umg.progra3.parcial1.pilas;
+
+import java.util.Iterator;
+
+public class NotationParser {
+
+    private Pila pila;
+
+    public NotationParser(Pila pila) {
+        this.pila = pila;
+    }
+
+    public String posFixToInfix(String expression){
+    	
+    	PilaImplementar letras= new PilaImplementar();
+    	
+    	
+    	
+        return expression;
+    }
+
+
+
+} 
